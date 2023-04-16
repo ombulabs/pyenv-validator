e83bab0b188cae89cff59cf88065a46c24d4ddae
Amanda Bizzinotto <amandabizzinotto@gmail.com>
2023-04-16 01:44:45 -0600

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="pyenv_validator",
-    version="0.1.0",
+    version="0.1.1",
     author="OmbuLabs - The Lean Software Boutique LLC",
     author_email="oss@ombulabs.com",
     desciption="Validate environment variables.",
