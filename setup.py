fbc278165b4d3637e7897773234c13d0b272be0e
Amanda Bizzinotto <amandabizzinotto@gmail.com>
2023-04-16 01:09:41 -0600

    Add static checks

--- setup.py
+++ setup.py
@@ -1,4 +1,5 @@
-from setuptools import setup, find_packages
+# noqa=D100
+from setuptools import find_packages, setup
 
 with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
